788f9d2a1b2eefa2360d648908c004f7748fc1cd
Andrew <AndrewKoerner.B@gmail.com>
2011-01-21 12:00:15 -0600

    cleaner version

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,13 +1,12 @@
 puts 'Welcome To FizzBuzz'
 
 
-
-for index in (1..100) do
-	if ((index % 3 == 0) && (index % 5 == 0))
+(1..100).each do |index|
+	if index % 3 == 0 && index % 5 == 0
 	   puts 'FizzBuzz'
-	elsif (index % 5 == 0)
+	elsif index % 5 == 0
 	   puts 'Buzz'
-	elsif (index % 3 == 0)
+	elsif index % 3 == 0
 	   puts 'Fizz'
 	else
 	   puts index
