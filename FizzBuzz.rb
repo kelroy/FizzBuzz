52f9e1a4c82a35201ab2e29da6b3ff5c7d92c286
Andrew <AndrewKoerner.B@gmail.com>
2011-01-21 11:54:44 -0600

    Valid Solution

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -3,12 +3,12 @@ puts 'Welcome To FizzBuzz'
 
 
 for index in (1..100) do
-	if ((index % 3 == 0) && (index % 10 != 0))
-	   puts 'Fizz'
-	elsif ((index % 10 == 0) and (index % 3 != 0))
-	   puts 'Buzz'
-	elsif ((index % 3 == 0) && (index % 10 == 0))
+	if ((index % 3 == 0) && (index % 5 == 0))
 	   puts 'FizzBuzz'
+	elsif (index % 5 == 0)
+	   puts 'Buzz'
+	elsif (index % 3 == 0)
+	   puts 'Fizz'
 	else
 	   puts index
 	end
