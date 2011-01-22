c54027ba8c614b121e4ebe953debadf78680afc3
Andrew <AndrewKoerner.B@gmail.com>
2011-01-22 10:41:15 -0600

    new class added

---
+++ FizzBuzz.class.rb
@@ -0,0 +1,25 @@
+Class FizzBuzz
+ attr_accessor :fizz, :buzz
+ 
+ #constructor#
+  def initialize()
+    @fizz = 'Fizz'
+    @buzz = 'Buzz'
+	puts 'Welcome To FizzBuzz'
+  end
+  
+   def speek(value)
+     if index % 3 == 0 && index % 5 == 0
+	   output = fizz + buzz
+	elsif index % 5 == 0
+	   output = buzz
+	elsif index % 3 == 0
+	  output = fizz
+	else
+	   output = value
+	end
+	 return output;
+   end
+  
+   
+end
