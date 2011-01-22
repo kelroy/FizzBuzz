92a049af04e18588b8d0fe2c0616a09f210d0568
Andrew <AndrewKoerner.B@gmail.com>
2011-01-22 11:20:22 -0600

    Final Solution

--- main.rb
+++ main.rb
@@ -1,11 +1,7 @@
 require 'FizzBuzz.class'
 fizzbuzz = FizzBuzz.new
 (1..100).each do |index|
-<<<<<<< HEAD
-	puts fizzbuzz.speek(index)
-=======
-	#puts index
+	#puts indexgit z.
 	puts fizzbuzz.speek(index) 
->>>>>>> FizzBuzz_oo
 
 end
